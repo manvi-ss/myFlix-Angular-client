9cfb0f4444b4de46275125511114e52825792a40
Manvi <131688388+manvi-ss@users.noreply.github.com>
2023-09-14 22:55:30 +0200

    user login  form created and implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -12,6 +13,13 @@ export class AppComponent {
   constructor(public dialog: MatDialog){}
 // This is the function that will open the dialog when the signup button is clicked  
   openUserRegistrationDialog(): void{
-    this.dialog.open(UserRegistrationFormComponent,{width: "280px"});// Assigning the dialog a width
+    this.dialog.open(UserRegistrationFormComponent,{
+      width: "280px" // Assigning the dialog a width
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px' // Assigning the dialog a width
+    });
   }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Observer } from 'rxjs';
+
+import { Router } from '@angular/router';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = {Username:'', Password: ''};
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+    
+    ) { }
+    ngOnInit(): void { }
+
+  /**
+   * on login token, userdata, and Username will be stored in localstorage. 
+   * user will be sent to the movie page 
+   */
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe({
+      next: (data) => {
+        localStorage.setItem('user', JSON.stringify(data.user));
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('Username', data.user.Username);
+  
+        this.dialogRef.close();
+        this.snackBar.open('You have been logged in', 'OK', {
+          duration: 2000
+        });
+      },
+      error: () => {
+        this.snackBar.open('Sorry something went wrong. Please try again', 'OK', {
+          duration: 2000
+        });
+      }
+    });
+  }
+}
